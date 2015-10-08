c4b3a5daed543ec716b5a14b94d927ba42012dcb
nilskingston <nilskingston@hanovernorwichschools.org>
2015-10-08 14:32:56 -0400

    Updated from Brython Server: 10/8/2015 2:32:57 PM

--- cryptography.py
+++ cryptography.py
@@ -9,25 +9,30 @@ Write and submit a program that encrypts and decrypts user data.
 
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
-
+from operator import add
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
 a = ""
 while a != "q":
     a = input("Enter e to encrypt, d to decrypt, or q to quit: ")
     if a == "e":
-        encripting = input("What do you want to encript?")
-        jumbler = input("What should the key be?")
+        encripting = input("What do you want to encript? ")
+        jumbler = input("What should the key be? ")
         list(encripting)
+        for x in encripting:
+            associations.find(x)
         list(jumbler)
-        jumbled = [sum(x) for x in zip(encripting, jumbler)]
-        print(jumbled)
+        for x in jumbler:
+            associations.find(x)
+        jumbled = map(add, encripting, jumbler)
+        
+        
       
     elif a == "d":
         jumbled_code = input("Message?")
         key = input("Key?")
         list(jumbled_code)
         list(key)
-        message = [x1 - x2 for (x1, x2) in zip(jumbled-code, key)]
+        message = [x1 - x2 for (x1, x2) in zip(jumbled_code, key)]
         
     elif a == "q":
         print("Goodbye!")
