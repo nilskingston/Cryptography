1dddaf789d70c425d2e8941ddde6b811306a072b
nilskingston <nilskingston@hanovernorwichschools.org>
2015-10-10 17:43:27 -0400

    Updated from Brython Server: 10/10/2015 5:43:26 PM

--- cryptography.py
+++ cryptography.py
@@ -23,8 +23,8 @@ while a != "q":
         j_list = []
         for x in jumbler:
             j_list.append(associations.find(x))
-        zip_list = zip(e_list, cycle(j_list)) if len(e_list) > len(j_list) else zip(cycle(e_list), j_list)
-        print(zip_list)
+        tuple_list = zip(e_list, cycle(j_list)) if len(e_list) > len(j_list) else zip(cycle(e_list), j_list)
+        print(tuple_list)
     elif a == "d":
         jumbled_code = input("Message?")
         key = input("Key?")
