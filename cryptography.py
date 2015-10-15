f2a45f597e1114870b6563c3c9e3bcff4f51bdfb
nilskingston <nilskingston@hanovernorwichschools.org>
2015-10-15 12:43:23 -0400

    Updated from Brython Server: 10/15/2015 12:43:24 PM

--- cryptography.py
+++ cryptography.py
@@ -10,6 +10,7 @@ Write and submit a program that encrypts and decrypts user data.
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
 #from itertools import cycle
+from itertools import cycle
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
 a = ""
 while a != "q":
